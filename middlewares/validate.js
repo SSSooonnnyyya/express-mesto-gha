66a31123b67de5656b2a2febc878d109f2a05289
Sonya Bagretsova <bigha@yandex.ru>
2023-06-04 20:27:16 +0300

    corrections

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -43,7 +43,7 @@ const validateCardBody = celebrate({
     name: Joi.string().required().min(2).max(30),
     link: Joi.string().required(),
     owner: Joi.string(),
-    likes: Joi.array().required().default([]),
+    likes: Joi.array().default([]),
     createdAt: Joi.date().default(Date.now),
   }),
 });
